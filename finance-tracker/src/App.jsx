f8ae794e9962e2a65c048dece6c68237560f1a26
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-09-20 21:57:11 -0500

    added useEffect

--- finance-tracker/src/App.jsx
+++ finance-tracker/src/App.jsx
@@ -5,4 +5,10 @@ function App() {
   const [expenses, setExpenses] = useState([]);
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/expenses")
+    .then(response => setExpenses(response.data))
+    .catch(error => console.error(error)); 
+  }, []);
 }
